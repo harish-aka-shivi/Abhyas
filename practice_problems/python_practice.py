6f26e35cc07d32ca2be3f935b5bf4cc586868178
harish <harishakashivi@gmail.com>
2017-05-24 10:52:10 +0530

    Getting some python practice

--- practice_problems/python_practice.py
+++ practice_problems/python_practice.py
@@ -151,3 +151,120 @@ print deep_count([[[[[[[[1, 2, 3]]]]]]]])
 #>>> 10
 
 #############################################################################
+ada_family = { 'Judith Blunt-Lytton': ['Anne Isabella Blunt', 'Wilfrid Scawen Blunt'],
+              'Ada King-Milbanke': ['Ralph King-Milbanke', 'Fanny Heriot'],
+              'Ralph King-Milbanke': ['Augusta Ada King', 'William King-Noel'],
+              'Anne Isabella Blunt': ['Augusta Ada King', 'William King-Noel'],
+              'Byron King-Noel': ['Augusta Ada King', 'William King-Noel'],
+              'Augusta Ada King': ['Anne Isabella Milbanke', 'George Gordon Byron'],
+              'George Gordon Byron': ['Catherine Gordon', 'Captain John Byron'],
+              'John Byron': ['Vice-Admiral John Byron', 'Sophia Trevannion'] }
+
+# Define a procedure, ancestors(genealogy, person), that takes as its first input
+# a Dictionary in the form given above, and as its second input the name of a
+# person. It should return a list giving all the known ancestors of the input
+# person (this should be the empty list if there are none). The order of the list
+# does not matter and duplicates will be ignored.
+
+def ancestors(genealogy, person):
+    if person not in genealogy:
+        return [];
+
+    a  = genealogy[person]
+    for item in a:
+        a = a + ancestors(genealogy,item)
+        #print item + "  - >item "
+    return a
+
+#Test Cases
+# Here are some examples:
+print ancestors(ada_family, 'Augusta Ada King')
+#>>> ['Anne Isabella Milbanke', 'George Gordon Byron',
+#    'Catherine Gordon','Captain John Byron']
+
+print ancestors(ada_family, 'Judith Blunt-Lytton')
+#>>> ['Anne Isabella Blunt', 'Wilfrid Scawen Blunt', 'Augusta Ada King',
+#    'William King-Noel', 'Anne Isabella Milbanke', 'George Gordon Byron',
+#    'Catherine Gordon', 'Captain John Byron']
+print ancestors(ada_family, 'Dave')
+#>>> []
+
+##############################################################################################################
+
+# Khayyam Triangle
+
+# The French mathematician, Blaise Pascal, who built a mechanical computer in
+# the 17th century, studied a pattern of numbers now commonly known in parts of
+# the world as Pascal's Triangle (it was also previously studied by many Indian,
+# Chinese, and Persian mathematicians, and is known by different names in other
+# parts of the world).
+
+# The pattern is shown below:
+
+#                    1
+#                   1 1
+#                  1 2 1
+#                 1 3 3 1
+#                1 4 6 4 1
+#                   ...
+
+# Each number is the sum of the number above it to the left and the number above
+# it to the right (any missing numbers are counted as 0).
+
+# Define a procedure, triangle(n), that takes a number n as its input, and
+# returns a list of the first n rows in the triangle. Each element of the
+# returned list should be a list of the numbers at the corresponding row in the
+# triangle.
+
+
+## By Recursion
+def triangleRecursion(n):
+
+    if n == 1:
+        return [[1]]
+
+    a = triangle(n-1)
+    # print "a is ", a
+    b = a[-1]
+    c = []
+    # print "b is equal",  b
+    start = 0
+    for item1 in range(0,len(b)):
+        c.append(start+b[item1])
+        start = b[item1]
+    c.append(start+0)
+    a.append(c)
+    return a
+
+
+def triangle(n):
+
+    if n == 0:
+        return []
+    ans = [[1]]
+    for item in range(2,n+1):
+        a = ans[item-2]
+        b = []
+        start = 0
+        for item1 in range(0,len(a)):
+            b.append(start+a[item1])
+            start = a[item1]
+        b.append(start+0)
+        ans.append(b)
+    return ans
+
+#Test Cases
+#print triangle(0)
+#>>> []
+
+#print triangle(1)
+#>>> [[1]]
+
+#print triangle(2)
+#>> [[1], [1, 1]]
+
+print triangle(3)
+#>>> [[1], [1, 1], [1, 2, 1]]
+
+print triangle(6)
+#>>> [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]
