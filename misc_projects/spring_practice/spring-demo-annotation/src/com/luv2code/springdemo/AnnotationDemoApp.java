c33624504c5dc5857ae680493adcabf654c59d86
harish <harishakashivi@gmail.com>
2017-10-06 08:28:18 +0530

    modified gitignore

---
+++ misc_projects/spring_practice/spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,28 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	
+	public static void main(String[] args) {
+		
+		// read spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	
+		
+		// get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach",Coach.class);
+
+		
+		// call the method on bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+	
+		
+		// close the context
+		context.close();
+		
+	}
+
+}
---
+++ misc_projects/spring_practice/spring-demo-annotation/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,41 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+	private FortuneService fortuneService;
+	
+	public TennisCoach()  {
+		System.out.println("inside def constructor");
+
+	}
+//	@Autowired
+//	public TennisCoach(FortuneService fortuneService) {
+//		this.fortuneService = fortuneService;
+//	}
+//	
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService fortuneService) {
+		System.out.println("inside do some crazy constructor");
+
+		this.fortuneService = fortuneService;
+	}
+	
+	public String getDailyWorkout() {
+		return "Dont get tennis elbow";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+	
+//	public void doSomeCrazyStuff() {
+//		
+//	}
+
+	
+	
+}
