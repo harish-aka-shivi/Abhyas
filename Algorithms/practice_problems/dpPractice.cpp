e46b5c5ccb51bdbb67a11969b98f9695d18ff5ee
harish <harishakashivi@gmail.com>
2017-10-30 10:35:12 +0530

    Added min to one problem in dp

--- Algorithms/practice_problems/dpPractice.cpp
+++ Algorithms/practice_problems/dpPractice.cpp
@@ -1,5 +1,93 @@
 #include <iostream>
 using namespace std;
+
+
+//----------------------------------------
+// Problem
+// Min Steps to one
+//given positive int n
+//make it 1 by 3 ways
+//- n-1
+//- n/2 ==> n%2 == 0
+//- n/3 ==> n%3 == 0
+//return number of steps
+
+// solution using normal recursion
+// repetitive calculation
+// TODO: implement memoization(dp)
+int min_step(int n) {
+  if( n == 1) {
+    return 0;
+  }
+  int ans = 1 + min_steps(n-1);
+
+  if(n % 2 == 0) {
+    ans = min_step(ans, 1+ min_step(n/2);
+  }
+
+  if(n % 3 == 0) {
+    ans = min_step(ans, 1 + min_step(n/3);
+  }
+}
+
+
+// using dp
+// bottom down approach
+// avoided repetitive calculation
+int minStepUsingDp(int n, int* arr) {
+  if( n == 1) {
+    return 0;
+  }
+
+  if(arr[n] > 0) {
+    return arr[n]
+  }
+
+  int ans = 1 + minStepUsingDp(n-1,arr);
+
+  if(n % 2 == 0) {
+    ans = min(ans, 1+ minStepUsingDp(n/2,arr);
+  }
+
+  if(n % 3 == 0) {
+    ans = min(ans, 1 + minStepUsingDp(n/3,arr);
+  }
+
+  arr[n] = ans;
+  return ans;
+}
+
+// using bottom up approach
+//  better space complexity
+// no recursion
+int minStep3(int n) {
+  int* arr = new int[n+1]
+  arr[1] = 0;
+  for(int i = 2; i <= n; i++) {
+    int ans = 1 + arr[i-1];
+    if(n % 2 == 0) {
+      ans = min(ans, 1+ minStep3(n/2,arr);
+    }
+
+    if(n % 3 == 0) {
+      ans = min(ans, 1 + minStep3(n/3,arr);
+    }
+    arr[i] = ans;
+  }
+}
+
+
+
+
+//------------------------------------------------
+int fibUsingRec(int n) {
+   if( n == 0 || n == 1) {
+       return 1;
+   }
+
+   return fib(n-1) + fib(n-2)
+}
+
 int fibUsingDp(int n, int* arr) {
    if( n == 0 || n == 1) {
        return 1;
